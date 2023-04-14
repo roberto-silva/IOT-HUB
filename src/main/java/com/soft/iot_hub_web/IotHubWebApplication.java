1170d8f593e442d7604ac562f13c106e822adc16
roberto-silva <roberto.silva.oli@outlook.com>
2023-04-14 18:21:11 -0300

    Initial commit

---
+++ src/main/java/com/soft/iot_hub_web/IotHubWebApplication.java
@@ -0,0 +1,13 @@
+package com.soft.iot_hub_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotHubWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotHubWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soft/iot_hub_web/IotHubWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.iot_hub_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotHubWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
